71ffec46c9090848547981f60d90b50f6b2d65fc
Etibar Abdullayev <abdullayev3tibar@gmail.com>
2021-03-06 20:47:58 +0400

    my old wordlist maker

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(369, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "WodList Duzelt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 232);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 309);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(242, 308);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "999";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Baslangic Sayi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Son Sayi:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 276);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(455, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Teal;
+            this.label3.Location = new System.Drawing.Point(12, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Melumatlari Daxil Edin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 333);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(493, 372);
+            this.MinimumSize = new System.Drawing.Size(493, 372);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WordList Duzelt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String txt = textBox1.Text;
+            String[] parcala = txt.Split(' ');
+           // int baslangic = Convert.ToInt32(textBox2.Text);
+            try{
+            int son = Convert.ToInt32(textBox3.Text);
+            for (int i = 0; i < parcala.Count(); i++)
+            {
+                for (int a = 0; a < parcala.Count(); a++)                  
+                {
+                    FileStream fayl = new FileStream("wordlist.txt", FileMode.Append);
+                    StreamWriter yaz = new StreamWriter(fayl);
+                    yaz.WriteLine(parcala[i] + parcala[a]);
+                    yaz.Close();
+                }
+                string sira = parcala[i];
+                FileStream fayl1 = new FileStream("wordlist.txt", FileMode.Append);   
+                StreamWriter yaz1 = new StreamWriter(fayl1);
+                yaz1.WriteLine(sira + "qwerty");
+                yaz1.WriteLine(sira + "admin");
+                yaz1.WriteLine(sira + "1234");
+                yaz1.WriteLine(sira + "12345");
+                yaz1.WriteLine(sira + "123456");
+                yaz1.WriteLine(sira + "1234567");
+                yaz1.WriteLine(sira + "12345678");
+                yaz1.WriteLine(sira + "123456789");
+                yaz1.WriteLine(sira + "001");
+                yaz1.WriteLine(sira + "002");
+                yaz1.WriteLine(sira + "003");
+                yaz1.WriteLine(sira + "004");
+                yaz1.WriteLine(sira + "005");
+                yaz1.WriteLine(sira + "006");
+                yaz1.WriteLine(sira + "007");
+                yaz1.WriteLine(sira + "008");
+                yaz1.WriteLine(sira + "009");
+                yaz1.WriteLine("001" + sira);
+                yaz1.WriteLine("002" + sira);
+                yaz1.WriteLine("003" + sira);
+                yaz1.WriteLine("004" + sira);
+                yaz1.WriteLine("005" + sira);
+                yaz1.WriteLine("006" + sira);
+                yaz1.WriteLine("007" + sira);
+                yaz1.WriteLine("008" + sira);
+                yaz1.WriteLine("009" + sira);
+                yaz1.WriteLine(sira + "01");
+                yaz1.WriteLine(sira + "02");
+                yaz1.WriteLine(sira + "03");
+                yaz1.WriteLine(sira + "04");
+                yaz1.WriteLine(sira + "05");
+                yaz1.WriteLine(sira + "06");
+                yaz1.WriteLine(sira + "07");
+                yaz1.WriteLine(sira + "08");
+                yaz1.WriteLine(sira + "09");
+                yaz1.WriteLine("01" + sira);
+                yaz1.WriteLine("02" + sira);
+                yaz1.WriteLine("03" + sira);
+                yaz1.WriteLine("04" + sira);
+                yaz1.WriteLine("05" + sira);
+                yaz1.WriteLine("06" + sira);
+                yaz1.WriteLine("07" + sira);
+                yaz1.WriteLine("08" + sira);
+                yaz1.WriteLine("09" + sira);
+                yaz1.WriteLine(sira + "1111");
+                yaz1.WriteLine(sira + "2222");
+                yaz1.WriteLine(sira + "3333");
+                yaz1.WriteLine(sira + "4444");
+                yaz1.WriteLine(sira + "5555");
+                yaz1.WriteLine(sira + "6666");
+                yaz1.WriteLine(sira + "7777");
+                yaz1.WriteLine(sira + "8888");
+                yaz1.WriteLine(sira + "9999");
+                yaz1.WriteLine(sira + "0101");
+                yaz1.WriteLine(sira + "0202");
+                yaz1.WriteLine(sira + "0303");
+                yaz1.WriteLine(sira + "0404");
+                yaz1.WriteLine(sira + "0505");
+                yaz1.WriteLine(sira + "0606");
+                yaz1.WriteLine(sira + "0707");
+                yaz1.WriteLine(sira + "0808");
+                yaz1.WriteLine(sira + "0909");
+                yaz1.WriteLine("0101" + sira);
+                yaz1.WriteLine("0202" + sira);
+                yaz1.WriteLine("0303" + sira);
+                yaz1.WriteLine("0404" + sira);
+                yaz1.WriteLine("0505" + sira);
+                yaz1.WriteLine("0606" + sira);
+                yaz1.WriteLine("0707" + sira);
+                yaz1.WriteLine("0808" + sira);
+                yaz1.WriteLine("0909" + sira);
+                yaz1.WriteLine("001001" + sira);
+                yaz1.WriteLine("002002" + sira);
+                yaz1.WriteLine("003003" + sira);
+                yaz1.WriteLine("004004" + sira);
+                yaz1.WriteLine("005005" + sira);
+                yaz1.WriteLine("006006" + sira);
+                yaz1.WriteLine("007007" + sira);
+                yaz1.WriteLine("008008" + sira);
+                yaz1.WriteLine("009009" + sira);
+                yaz1.WriteLine("001001" + sira);
+                yaz1.WriteLine("002002" + sira);
+                yaz1.WriteLine("003003" + sira);
+                yaz1.WriteLine("004004" + sira);
+                yaz1.WriteLine("005005" + sira);
+                yaz1.WriteLine("006006" + sira);
+                yaz1.WriteLine("007007" + sira);
+                yaz1.WriteLine("008008" + sira);
+                yaz1.WriteLine("009009" + sira);
+                yaz1.Close();
+                for (int baslangic = Convert.ToInt32(textBox2.Text); baslangic < son; baslangic++)  //sayi araligi duzeldir(birlesdirir)
+                {
+                    String soz = sira + baslangic;
+                    String soz2 = baslangic + sira;
+                    FileStream fayl = new FileStream("wordlist.txt", FileMode.Append);
+                    StreamWriter yaz = new StreamWriter(fayl);
+                    yaz.WriteLine(soz);
+                    yaz.WriteLine(soz2);
+                    yaz.Close();
+                }
+                progressBar1.Maximum = parcala.Count() -1;
+
+                progressBar1.Value = i;
+            }
+            }
+            catch {
+                MessageBox.Show("Sayilar Daxil Edin Meselen 0 dan 999 kimi");
+            }
+            if (progressBar1.Value == parcala.Count() - 1)
+            {
+                MessageBox.Show("Wordlist Hazırdır");
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
